3b352d8580ec00544b8dec3bbcf9526cc8dd2362
Emre Taştekin <112701080+emretastekin@users.noreply.github.com>
2025-05-11 12:45:11 +0300

    commit.

--- chatbotAI/app/src/main/java/com/emretastekin/chatbotaibootcamp/MainActivity.kt
+++ chatbotAI/app/src/main/java/com/emretastekin/chatbotaibootcamp/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello $name!!!",
         modifier = modifier
     )
 }
